14282fddaa292e2416138835b4e672918b655183
Sam-MARTis <samanthmartis@gmail.com>
2024-07-02 19:38:07 +0530

    basic getting started

---
+++ Learning/00_basics/basic.jl
@@ -0,0 +1,21 @@
+v1 = 3
+v2 = 4
+println("Hello world")
+println(v1+v2)
+
+println(pi) #prints value of pi. Its a builtin constant
+println(2pi) #interpreted as 2*pi
+println(round(sqrt(100)))
+
+v1 = 4
+print(v1)
+
+v3::String="diff"
+
+
+
+
+
+
+
+# v3 = 4 #Will throw an error
