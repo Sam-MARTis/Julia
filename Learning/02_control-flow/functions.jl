6a610140ca15efb2577ff6b6a990f889b0e28685
Sam-MARTis <samanthmartis@gmail.com>
2024-07-03 10:31:12 +0530

    Added more function types

--- Learning/02_control-flow/functions.jl
+++ Learning/02_control-flow/functions.jl
@@ -67,3 +67,35 @@ end
 
 println(getSumOverload(3, 4))
 println(getSumOverload("2", 3))
+
+
+
+
+# Anonymous functions
+
+v1 = map(x-> x^3, [1,2,3,4])
+
+println(v1)
+
+v2 = map((x, y)-> x+y, [1,2,3], [5, 9, 14])
+println(v2)
+
+
+v4 = reduce(+, 1:100)
+println(v4)
+
+
+sentance = "This is a test sentance here"
+sArray = split(sentance)
+
+longest = reduce((x, y)-> length(x)>length(y) ? x : y , sArray)
+
+println(longest)
+
+println(2hypot(3, 4))
+println(hypot(90, 90))
+
+
+a40 = [1,2,3]
+println(a40.+a40)
+println(a40.* 4)
