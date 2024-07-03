6086d87fc64d2ce9d06837f6b4b3740f16a7dd00
Sam-MARTis <samanthmartis@gmail.com>
2024-07-03 09:30:19 +0530

    Added sets

--- Learning/01_types/tuples_and_dictionaries_and_sets.jl
+++ Learning/01_types/tuples_and_dictionaries_and_sets.jl
@@ -20,8 +20,8 @@ println(d1["pi"])
 
 d1["golden"] = 1.618
 delete!(d1, "pi")
-haskey(d1, "pi")
-println(in(d1, "pi"=>3.14))
+println(haskey(d1, "pi"))
+println(in("pi"=>3.14, d1))
 
 println(keys(d1))
 println(values(d1))
@@ -34,3 +34,15 @@ end
 for(key, value) in d1
     println(value)
 end
+
+
+st1 = Set(["A", "B", "C"])
+println(st1)
+
+push!(st1, "D")
+println(in("B", st1))
+
+st2 = Set(["B", "C", "D", "E"])
+println(union(st1, st2))
+println(intersect(st1, st2))
+println(setdiff(st1, st2))
