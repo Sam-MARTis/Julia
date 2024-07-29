d99e9d1fc7b1314b26bde62c9aaa727f1cd09f35
Sam-MARTis <samanthmartis@gmail.com>
2024-07-29 14:54:24 +0530

    Added sequential adding function

--- Learning/04_GPU/02_Basic-Computation.jl
+++ Learning/04_GPU/02_Basic-Computation.jl
@@ -1,15 +1,30 @@
+print("\033c")
 N = 2^20
 x = fill(1.0f0, N) #float32
 y = fill(2.0f0, N)
 
-y .+= x
+# y .+= x
 
-# The above are performed on cpu
+
 
 
 using Test
+# @test all(y .== 3.0f0)
+
+
+
+function sequential_add!(y, x)
+    for i ∈ eachindex(y, x)
+        @inbounds y[i] += x[i]
+    end
+    return nothing
+end
+# The above are performed on cpu
+
+
+fill!(y, 2)
+sequential_add!(y, x)
+
 @test all(y .== 3.0f0)
-
-
 
 
